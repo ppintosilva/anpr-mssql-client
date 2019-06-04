0d69647262cd950c3bb487ebf8f0f0dd6f8458f6
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2019-06-04 08:31:10 +0100

    Simplified get raw npdata query

--- queries/raw_npdata.sql
+++ queries/raw_npdata.sql
@@ -6,15 +6,10 @@ SELECT
     d.VRN as vehicle,
     d.CameraID as camera,
     d.Timestamp as timestamp,
-    d.VRNConfidence as confidence,
-    d.TimestampError as timestamp_error,
-    d.PlateNotRead as was_read,
-    dir.Description as direction
+    d.VRNConfidence as confidence
 
 FROM
     data.NumberPlateData as d
-INNER JOIN
-    static.VehicleDirectionType as dir ON d.VehicleDirectionID = dir.VehicleDirectionTypeID
 WHERE
     d.Timestamp >= "{{ start }}" AND d.Timestamp < "{{ end }}"
 
